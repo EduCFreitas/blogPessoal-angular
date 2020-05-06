9ed50c7b1c775033d8d1d54855b54977571ba68c
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-05-06 16:51:09 -0300

    Implementação do findByTitulo

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -8,47 +8,55 @@ import { Postagem } from '../model/Postagem';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
-
+  
   key= 'data'
-
+  
   reverse = true
-
+  
   listaPostagens: Postagem[]
-
+  
   postagem: Postagem = new Postagem
-
+  
   alerta:boolean = false
-
+  
+  titulo:string
+  
   constructor(private postagemService:PostagemService) { }
-
+  
   ngOnInit() {
     this.findAllPostagens()
-
+    
     let item:string = localStorage.getItem('delOk')
     if (item === "true"){
       this.alerta = true
       localStorage.clear()
-
+      
       setTimeout(()=>{
         location.assign('/feed')
       }, 3000)
-
+      
     }
-
+    
     // window.scroll(0, 0)
   }
-
+  
   findAllPostagens(){
     this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
       this.listaPostagens = resp
     })
   }
-
+  
   publicar(){
     this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
-       this.postagem = resp
-       location.assign('/feed')
+      this.postagem = resp
+      location.assign('/feed')
     })
-}
+  }
 
+  pesquisarPorTitulo(){
+    this.postagemService.findByTitulo(this.titulo).subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+  
 }
